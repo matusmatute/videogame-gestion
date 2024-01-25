f0c9c2995b37ccf55999a394b6be7486a5b6e71a
Laura <laura@netcommerce.mx>
2024-01-25 15:45:29 -0600

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -33,4 +34,4 @@ Route::resource('metadata', App\Http\Controllers\MetadataController::class)
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
