450c28afbf27125caf0bf91891631240edc2331a
Laura <laura@netcommerce.mx>
2024-01-26 09:40:32 -0600

    instalacion de livewire

--- resources/views/console/create.blade.php
+++ resources/views/console/create.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200">
                 <form action="{{ route('console.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     @php
